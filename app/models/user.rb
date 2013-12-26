237353b0316c1356b38c3913c4bf05462b057e23
Alex Behlen <a.behlen@gmail.com>
2013-12-26 11:49:31 -0600

    Finished user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accessor :password
+	#attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
 	before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 } 
