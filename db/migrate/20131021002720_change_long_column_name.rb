2394aca0a870bb7323e2704af0c8510e9187caae
albs22 <a.behlen@gmail.com>
2014-01-11 13:38:42 -0700

    Remove unused migrations

--- db/migrate/20131021002720_change_long_column_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeLongColumnName < ActiveRecord::Migration
-  def up
-    remove_column :violations, :long, :float
-  end
-
-  def down
-    add_column :violations, :lng, :float
-  end
-end
--- db/migrate/20131023042911_re_add_lng_column.rb
+++
@@ -1,8 +0,0 @@
-class ReAddLngColumn < ActiveRecord::Migration
-  def up
-    add_column :violations, :lng, :float
-  end
-
-  def down
-  end
-end
