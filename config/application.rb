39e38fd7b8ebc8f3d9b8ab1fa2b16b72ddc00d97
albs22 <a.behlen@gmail.com>
2014-09-20 12:55:15 -0700

    Fixed Bootstrap glyph issue

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module CodeViolations
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
+    #Fix boot strap glyph font issue
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
