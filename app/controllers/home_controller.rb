313731b1fb4f29bcb911c17afa7bf05c1dbc3d33
Alex <a.behlen@gmail.com>
2015-03-29 12:53:35 -0700

    Resonsive design tweaks, fixed home page mess query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
      @recent_violations = Violation.where(:approved => 't', :status => 'open').last(2).reverse
      @clean_count = Violation.where(:status => 'closed').count
-     @recent_events = Event.order('date').first(1)
+     @recent_events = Event.upcoming.order('date').first(1)
   end
 end
